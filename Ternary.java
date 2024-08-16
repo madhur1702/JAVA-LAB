fffed51b082e6ff48b711768b916f2875ef97f99
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2024-08-16 20:05:00 +0530

    Create Ternary.java

---
+++ Ternary.java
@@ -0,0 +1,14 @@
+class Ternary{
+public static void main(String[]args){
+int i,k;
+i=10;
+k=i<0?-i:i;
+System.out.println("Absolute value is:");
+System.out.println(i+"\t is \t"+k);
+
+i=-10;
+k=i<0?-i:i;
+System.out.println("Absolute Value of:");
+System.out.println(i +"\t is \t"+k);
+}
+}
