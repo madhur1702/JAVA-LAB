2dbe7f1f53c534cdd20c6c4e958d93bac9516a24
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2024-09-27 23:01:14 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,12 @@
+class Test
+{
+public static void main(String[] args)
+{
+int i = 100;
+ long l = i;
+ float f = l;
+System.out.println("Int value "+i);
+System.out.println("Long value "+l);
+System.out.println("Float value "+f);
+}
+}
